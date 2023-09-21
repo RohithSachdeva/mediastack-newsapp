1ef234f2b1900c146c59dbe1572c9ad2350770d6
RohithSachdeva <rsachdeva@ucdavis.edu>
2023-09-20 21:11:41 -0700

    Setup Stepzen.  Before implementing FETCH call

--- utils/fetchNews.tsx
+++ utils/fetchNews.tsx
@@ -1,5 +1,38 @@
+import { gql } from "graphql-request";
+
 const fetchNews = async (
   category?: Category | string,
   keywords?: string,
   isDynamic?: boolean
-) => {};
+) => {
+  const query = gql`
+    query MyQuery($access_key: String!, $category: String!, $keywords: String) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "us"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        data {
+          author
+          category
+          description
+          country
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+      }
+    }
+  `;
+};
