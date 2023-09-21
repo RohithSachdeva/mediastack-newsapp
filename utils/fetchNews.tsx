ec47f8fe61455b836783984025670bd47a570792
RohithSachdeva <rsachdeva@ucdavis.edu>
2023-09-20 21:31:12 -0700

    Created fetchNews .. Before confirming it works

--- utils/fetchNews.tsx
+++ utils/fetchNews.tsx
@@ -35,4 +35,27 @@ const fetchNews = async (
       }
     }
   `;
+
+  const res = await fetch(
+    `https://yicheng.stepzen.net/api/loitering-hedgehog/__graphql`,
+    {
+      method: "POST",
+      cache: isDynamic ? "no-cache" : "default",
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Apikey ${process.env.STEPZEN_APIKEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_APIKEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+  console.log("Loading", category, keywords);
+  const newsResponse = await res.json();
 };
